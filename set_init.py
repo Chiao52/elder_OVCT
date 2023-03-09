1a1181907dff85b9bd98fe207886eb095e264424
momo8042 <88641414+momo8042@users.noreply.github.com>
2023-03-10 01:38:35 +0800

    Update set_init.py

--- set_init.py
+++ set_init.py
@@ -1,13 +1,19 @@
 import os
 
 def init_file_name():
-    dirPath = r'./functions'
-    file_list = os.listdir(dirPath)
+    dirPath = os.getcwd().replace('\\', '/')
+    dirPath = str(dirPath) + '/fuctions'
+    file_list = [f for f in os.listdir(dirPath) if os.path.isfile(os.path.join(dirPath, f))]
     file_name = []
     for i in file_list:
       j = i.strip('.py')
       file_name.append(j)
 
+    file_name.remove("__init__")
+    for i in file_name:
+        if ".sw" in i:
+            file_name.remove(i)
+        
     #write ./function/__init__.py
     file = open('./functions/__init__.py', 'w')
     file.write("__all__ = ")
