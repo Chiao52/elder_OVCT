0cff6b87a8e2cfd7c519cbe874f788edfa6aa1d5
Chiao52 <88649220+Chiao52@users.noreply.github.com>
2023-03-10 00:59:06 +0800

    Update file_storage.py

--- functions/file_storage.py
+++ functions/file_storage.py
@@ -9,7 +9,7 @@ def file_update(Source):
 
 def save_file(Source):
     print('\n-----------------------------------\n')
-    file_name = input("【 Please input the CSV file name that you want to save. 】 \n\nPlease enter the absolute file name\n\n=> ")
+    file_name = input("【 Please input the CSV file name that you want to save. 】 \n\nPlease enter the file name\n\n=> ")
     Path = './resources/' + file_name +'.csv'
     # columns = ['HostName', 'Country', 'IP', 'Speed (Mbps)', 'OpenVPN_ConfigData_Base64']
     # source_CSV = Source.reindex(columns=columns)
